812f43cb7c65e321ecde4636a354c8a3a3648a72
Mario Reder <mario.reder@pm.me>
2026-03-28 16:54:52 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -16,7 +16,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_WALLETCONNECT_PROJECT_ID": "",
   "VITE_APP_NAME": "Yahyaibr",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
-  "VITE_HAS_PRIMARY_LOGO": "false",
+  "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
   "VITE_CUSTOM_MENUS": "",
